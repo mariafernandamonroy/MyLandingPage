09277ec7c4e344e8b0f31cc20bcc25309f61da49
María Fernanda Monroy <maria.monroy@sofka.com.co>
2022-09-15 13:10:51 -0500

    añadiendo colores al carousel

--- src/components/PortafolioCarousel.jsx
+++ src/components/PortafolioCarousel.jsx
@@ -1,10 +1,26 @@
-import Carousel from 'react-material-ui-carousel'
+import Carousel from "react-material-ui-carousel";
 import { proyectos } from "../database/proyectos";
 import { PortafolioCard } from "./PortafolioCard";
 
 export const PortafolioCarousel = () => {
   return (
-    <Carousel animation='fade' duration={700}>
+    <Carousel
+      animation="fade"
+      duration={700}
+      navButtonsAlwaysVisible={true}
+      navButtonsProps={{
+        style: {
+          color: "white",
+          backgroundColor: "#003547",
+        },
+      }}
+      indicatorIconButtonProps={{
+        style: {
+          padding: "2px",
+          color: "#003547",
+        },
+      }}
+    >
       {proyectos.map((proyecto) => (
         <PortafolioCard key={proyecto.id} proyecto={proyecto} />
       ))}
